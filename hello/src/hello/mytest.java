801a91b1cdf2d6f97885445ab07ddcf79dfa04ee
dely007 <1176322485@qq.com>
2019-08-20 12:18:35 +0800

    修改Java类

--- hello/src/hello/mytest.java
+++ hello/src/hello/mytest.java
@@ -4,6 +4,6 @@ public class mytest {
 
 	public static void main(String[] args) {
 		System.out.println("nihao shijie");
-		System.out.println("nihao shijie");
+		System.out.println("nihao shijie haha");
 	}
 }
